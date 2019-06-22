e5461a618846b1058532f24af9549e0272e51f18
ribamar <ribamarop@gmail.com>
2019-06-21 21:07:06 -0300

    tentar fazer as screenshot amumentar

--- script.js
+++ script.js
@@ -249,3 +249,23 @@ function loadAnima(){
     document.getElementById("review-filme").innerHTML = animacao["review"];
   })
 }
+
+// Java Das screenshots
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+} 
